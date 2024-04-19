df17f1ce6ad758e0e9acf4f161b4fec6ee6612e4
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-19 14:25:08 +0300

    chenge for href

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
-
+import {locales, localePrefix} from './navigation';
  
 export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'uk', 'ru'],
- 
-  // Used when no locale matches
+  localePrefix,
+  locales,
   defaultLocale: 'en'
 });
  
