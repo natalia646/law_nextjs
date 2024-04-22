391844fee3946a94dd7206f51e16a9725057f26d
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-22 18:02:26 +0300

    json fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
  
 const withNextIntl = createNextIntlPlugin();
  
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/* @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+};
  
 export default withNextIntl(nextConfig);
