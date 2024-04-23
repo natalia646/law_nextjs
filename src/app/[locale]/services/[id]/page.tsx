ba372e840891a84d9ef749094d86d47b78ce0489
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-23 18:07:18 +0300

    chenge get up to messages

--- src/app/[locale]/services/[id]/page.tsx
+++ src/app/[locale]/services/[id]/page.tsx
@@ -1,12 +1,10 @@
-import getServeresList from "@/functions/getServercesList";
-import { useTranslations } from "next-intl";
 
-export default function SpecificServicePage({
-  params,
-}: {
-  params: { id: number };
-}) {
-  const services = getServeresList();
-  const t = useTranslations()
-  return <div>{t(`${services[params.id].title}`)}</div>;
+
+export default function Service({ params }: { params: { id: number } }) {
+  return (
+    <div>
+      {params.id}
+    </div>
+  )
 }
+
--- src/app/[locale]/services/page.jsx
+++ src/app/[locale]/services/page.jsx
@@ -1,17 +1,17 @@
-import getServeresList from "@/functions/getServercesList";
 import { Link } from "@/navigation";
+import { useMessages } from "next-intl";
 import styled from "./services.module.css";
-import { useTranslations } from "next-intl";
 
-export default function ServicesPage() {
-  const servicesList = getServeresList();
-  const t = useTranslations();
+export default function Services() {
+  const messeges = useMessages();
 
   return (
-    <nav>
-      {servicesList.map((item) => (
-        <Link href={`/services/${item.id}`} key={item.id}>{t(`${item.title}`)}</Link>
+    <div className={styled.links}>
+      {messeges.Services.map((item) => (
+        <Link href={`/services/${item.id}`} key={item.id}>
+          {item.title}
+        </Link>
       ))}
-    </nav>
+    </div>
   );
 }
