80a4d110f85e9c275b974a9da23b512359c4a6d0
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-23 18:05:32 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
  
 const withNextIntl = createNextIntlPlugin();
  
-/* @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
 };
