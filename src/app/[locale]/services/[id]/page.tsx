2e5db2e76d2d015dbf3ce8c317d309d1a642d7a9
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-24 19:00:52 +0300

    fix

--- src/app/[locale]/services/[id]/page.tsx
+++ src/app/[locale]/services/[id]/page.tsx
@@ -1,12 +1,1 @@
-import getServeresList from "@/functions/getServercesList";
-import { useTranslations } from "next-intl";
 
-export default function SpecificServicePage({
-  params,
-}: {
-  params: { id: number };
-}) {
-  const services = getServeresList();
-  const t = useTranslations();
-  return <p>{t(`${services[params.id].title}`)}</p>;
-}
