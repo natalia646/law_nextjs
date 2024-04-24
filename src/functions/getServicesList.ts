d581c2af6c29fc1ac4be470a5bcf45ba02756c4d
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-24 18:14:16 +0300

    create team list

---
+++ src/functions/getServicesList.ts
@@ -0,0 +1,12 @@
+import { useMessages } from "next-intl";
+import { ServiceType } from '../global';
+
+export default function getServicesList() {
+  const messeges = useMessages();
+  if (!Array.isArray(messeges.Services)) {
+    return [];
+  }
+  const servicesList: ServiceType[] = messeges.Services;
+
+  return servicesList;
+}
