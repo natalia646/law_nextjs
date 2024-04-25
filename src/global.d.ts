d2315cd51033962117d40c446400d09221ef6f37
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-25 11:17:16 +0300

    added type of team

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,21 @@ export interface ServiceType {
   id: number;
   title: string;
 }
+
+export interface MemberType {
+  id: number;
+  name: string;
+  position: string;
+  description: string;
+}
+export interface CommonInMembersType {
+  id:           number;
+  image:        string;
+  network:      NetworkOfMember;
+  certificates: string[];
+}
+
+export interface NetworkOfMember {
+  linkedin: string;
+  github:   string;
+}
