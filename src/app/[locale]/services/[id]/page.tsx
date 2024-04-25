18c7755f4abf719aba70dcd7d8604db29cb75fb8
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-25 18:48:04 +0300

    fix intl bug

--- src/app/[locale]/services/[id]/page.tsx
+++ src/app/[locale]/services/[id]/page.tsx
@@ -1,10 +1,8 @@
 import getServerList from "@/functions/getServicesList";
-import { useTranslations } from "next-intl";
 
 export default function ServicePage({ params }: { params: { id: number } }) {
   const services = getServerList();
-  const t = useTranslations()
   const { id } = params;
 
-  return <p>{t(`${services[id].title}`)}</p>;
+  return <p>{services[id].title}</p>;
 }
