d44ddf4372431681a383cc73eb57e0afa3bf5054
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-26 14:40:09 +0300

    Hoorey! metadeta worked

--- src/app/[locale]/services/[id]/page.tsx
+++ src/app/[locale]/services/[id]/page.tsx
@@ -1,4 +1,29 @@
 import getServerList from "@/functions/getServicesList";
+import type { Metadata } from "next";
+import { getMessages } from "next-intl/server";
+import { ServiceType } from "@/global";
+
+// export const metadata: Metadata = {
+//   title: '...',
+//   description: '...',
+// }
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: number };
+}): Promise<Metadata> {
+  const messages = await getMessages();
+  const { id } = params;
+  if (!Array.isArray(messages.Services)) {
+    return {};
+  }
+  const servicesList: ServiceType[] = messages.Services;
+
+  return {
+    title: servicesList[id].title,
+  };
+}
 
 export default function ServicePage({ params }: { params: { id: number } }) {
   const services = getServerList();
