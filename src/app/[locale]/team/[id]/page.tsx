c341d5ba866e5242a99ae6e932c0e14014d3a084
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-04-29 20:57:04 +0300

    added tags for member page

--- src/app/[locale]/team/[id]/page.tsx
+++ src/app/[locale]/team/[id]/page.tsx
@@ -1,7 +1,20 @@
+
 import getMembersList from "@/functions/getMembersList";
+import Image from "next/image";
+import styled from './member.module.css'
 
 export default function MemberPage({ params }: { params: { id: number } }) {
+
   const team = getMembersList();
   const { id } = params;
-  return <div>{team[id].name}</div>;
+  const {name, position, description, image } = team[id];
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{position}</p>
+      <p>{description}</p>
+      <Image src= {`/team/${image}`} alt={name} className={styled.image} width="700" height="700"/>
+    </div>
+  );
 }
