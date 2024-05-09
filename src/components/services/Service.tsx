00d0df12d14b75dad8fec15f89409d4981211f2b
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-09 16:56:53 +0300

    fix service

--- src/components/services/Service.tsx
+++ src/components/services/Service.tsx
@@ -1,16 +1,22 @@
 import styled from "./service.module.css";
 import { Link } from "@/navigation";
 import { ServiceType } from "../../global";
+import getCorrectNumber from "@/functions/getCorrectNumberOfServices";
 
 interface Props {
   item: ServiceType;
 }
 
+
 export default function Service({ item }: Props) {
+  const { id, title } = item;
 
   return (
-    <Link href={`/services/${item.id}`} className={styled.link}>
-      {item.title}
-    </Link>
+    <section className={styled.block}>
+      <p>{getCorrectNumber(item)}</p>
+      <Link href={`/services/${id}`} className={styled.link}>
+        {title}
+      </Link>
+    </section>
   );
 }
