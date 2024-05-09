37cb773d0b0ed9ac8b2e0631e35a887cc3eabc94
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-09 16:56:36 +0300

    fix

--- src/components/tedaliniLogo/TedaliniLogo.tsx
+++ src/components/tedaliniLogo/TedaliniLogo.tsx
@@ -3,18 +3,18 @@ import { Link } from "@/navigation";
 import { Gemunu_Libre } from "next/font/google";
 import styled from "./TedaliniLogo.module.css";
 
+
 const libre_font = Gemunu_Libre({
   weight: "600",
   subsets: ["latin"],
 });
 
-export default function TedaliniLogo() {
+export default function TedaliniLogo () {
   return (
     <Link href={`/`} className={`${styled.logo} ${libre_font.className}`}>
       <Image
         src={"/icons/logo/tedalini.svg"}
         alt="tedalini_logo"
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         width={32}
         height={32}
         className={styled.image}
