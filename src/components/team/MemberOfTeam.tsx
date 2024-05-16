90f0c5657c9140b8cc2baa2c50c0cf23859747c9
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-16 19:23:13 +0300

    create member component

--- src/components/team/MemberOfTeam.tsx
+++ src/components/team/MemberOfTeam.tsx
@@ -1,27 +1,44 @@
 import { Link } from "@/navigation";
 import { MemberType } from "../../global";
 import Image from "next/image";
-import style from "./memberOfTeam.module.css";
+
+import style from "./memberOfTeam.module.scss";
 
 interface Props {
   item: MemberType;
 }
 
 export default function MemberOfTeam({ item }: Props) {
-  const { id, name, position, description, image, certificates, networks } =
-    item;
+  const { id, name, position, image, networks } = item;
 
   return (
-    <div className={style.image_block}>
-      <Link href={`/team/${id}`}>
-        <Image
-          src={`/team/${image}`}
-          alt={name}
-          className={style.image}
-          width="700"
-          height="700"
-        />
-      </Link>
+    <div className={style.card}>
+      <div className={style.image_block}>
+        <Link href={`/team/${id}`}>
+          <section
+            className={style.section}
+            style={{
+              backgroundImage: `url(/team/${image})`,
+              backgroundSize: "cover",
+              width: "100%",
+              maxWidth: "305px",
+              minHeight: "297px",
+              height: "100%",
+            }}
+          ></section>
+        </Link>
+      </div>
+   <div className={style.p}>
+        <p>{name}</p>
+        <p>{position}</p>
+        <div>
+          {networks.map((val) => (
+            <Link href={val.link}>
+              <Image src={`/icons/networks/${val.icon}.svg`} alt="netvork" width={30} height={30}></Image>
+            </Link>
+          ))}
+        </div>
+   </div>
     </div>
   );
 }
