3e1e6bfe496bc834470691f4db1d5583d3d56786
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-16 15:39:15 +0300

    added some styles

--- src/components/team/MemberOfTeam.tsx
+++ src/components/team/MemberOfTeam.tsx
@@ -1,14 +1,27 @@
 import { Link } from "@/navigation";
-import {MemberType} from '../../global'
+import { MemberType } from "../../global";
 import Image from "next/image";
+import style from "./memberOfTeam.module.css";
 
 interface Props {
-    item: MemberType;
-  }
+  item: MemberType;
+}
 
-export default function MemberOfTeam({item} : Props) {
+export default function MemberOfTeam({ item }: Props) {
+  const { id, name, position, description, image, certificates, networks } =
+    item;
 
   return (
-    <Link href={`/team/${item.id}`}> {item.name} </Link>
-  )
+    <div className={style.image_block}>
+      <Link href={`/team/${id}`}>
+        <Image
+          src={`/team/${image}`}
+          alt={name}
+          className={style.image}
+          width="700"
+          height="700"
+        />
+      </Link>
+    </div>
+  );
 }
