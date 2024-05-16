6de567e2f02c7f17abb8a4f24998492c4fc22d15
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-16 19:24:00 +0300

    fix team page

--- src/app/[locale]/team/page.tsx
+++ src/app/[locale]/team/page.tsx
@@ -1,14 +1,22 @@
 import MemberOfTeam from "../../../components/team/MemberOfTeam";
 import getMembersList from "../../../functions/getMembersList";
-
+import style from "./teamPage.module.css";
+import { useTranslations } from "next-intl";
 
 export default function TeamPage() {
   const team = getMembersList();
+  const t = useTranslations("TeamPage");
   return (
-    <div>
-      {team.map((item) => (
-        <MemberOfTeam item={item} key={item.id} />
-      ))}
-    </div>
+    <article className={style.container}>
+      <div className={style.block}>
+        <div className={style.text}>
+          <h2>{t("our-team")}</h2>
+          <p>{t("description")}</p>
+        </div>
+        {team.map((item) => (
+          <MemberOfTeam item={item} key={item.id} />
+        ))}
+      </div>
+    </article>
   );
 }
