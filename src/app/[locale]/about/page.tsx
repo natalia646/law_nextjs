593748a7ff27b42635808a385d59a92818f428d9
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-17 13:14:38 +0300

    about page - should overwork

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,16 +1,24 @@
 import { useTranslations } from "next-intl";
 import TeamPage from "../team/page";
 import { Link } from "@/navigation";
+// import style from "./aboutPage.module.scss";
+// import Image from "next/image";
 
 export default function About() {
   const t = useTranslations("AboutPage");
 
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <TeamPage/>
+    <>
+      <article>
+        <section>
+          <h2>{t("title")}</h2>
+          <p>{t("desc1")}</p>
+          <p>{t("desc2")}</p>
+        </section>
+      </article>
+
+      <TeamPage />
       <Link href={`/team`}>{t("all-team")}</Link>
-      {/* <Link href={`/certificates`}>certificates</Link> */}
-    </div>
+    </>
   );
 }
