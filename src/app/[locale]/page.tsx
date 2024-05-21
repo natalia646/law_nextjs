e2f64e40ebe736fbc906bef6a7faf524423d656c
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-21 19:03:15 +0300

    added main screen to homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
+import MainScreen from "@/components/homePage/MainScreen";
 import RunningText from "@/components/runningText/RunningText";
 
 export default function HomePage() {
   return (
     <>
+      <MainScreen />
       <RunningText />
     </>
   );
