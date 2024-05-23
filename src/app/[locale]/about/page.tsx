47807e909a2dca584803d3c227a311c2dc55b238
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-24 00:20:21 +0300

    create part of team for about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,13 +1,13 @@
 import Bloks from "@/components/bloks/Bloks";
 import MainScreen from "@/components/forAboutPage/MainScreen";
-
+import PartOfTeam from "@/components/forAboutPage/partOfTeam";
 
 export default function AboutPage() {
   return (
     <>
-      <MainScreen/>
-      <Bloks/>
+      <MainScreen />
+      <Bloks />
+      <PartOfTeam />
     </>
-  )
+  );
 }
-
---
+++ src/components/forAboutPage/PartOfTeam.tsx
@@ -0,0 +1,27 @@
+import MemberOfTeam from "@/components/team/MemberOfTeam";
+import getMembersList from "../../functions/getMembersList";
+import { useTranslations } from "next-intl";
+import { Link } from "@/navigation";
+import style from './partOfTeam.module.scss'
+
+export default function PartOfTeam() {
+  const team = getMembersList();
+  const [m1, m2, m3, m4] = team;
+  const t = useTranslations("TeamPage");
+  return (
+    <article className={style.container}>
+      <div className={style.block}>
+        <div className={style.text}>
+          <h2>{t("our-team")}</h2>
+          <h6>{t("description")}</h6>
+          <Link href={"/team"} className={style.link}>
+            {t("view-all") + ' >'}
+          </Link>
+        </div>
+        {[m1, m2, m3, m4].map((item) => (
+          <MemberOfTeam item={item} key={item.id} />
+        ))}
+      </div>
+    </article>
+  );
+}
