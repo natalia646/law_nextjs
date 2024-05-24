34306fee9a6c2ff10033c15474a7a9b385595013
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-24 18:11:32 +0300

    fix path

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,6 +1,7 @@
 import Bloks from "@/components/bloks/Bloks";
 import MainScreen from "@/components/forAboutPage/MainScreen";
-import PartOfTeam from "@/components/forAboutPage/partOfTeam";
+import PartOfTeam from "@/components/forAboutPage/PartOfTeam";
+
 
 export default function AboutPage() {
   return (
