f2863f587d3c0a62719f1d81502a67be7d5d958b
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-24 15:08:59 +0300

    created partner component

---
+++ src/components/partners/Partners.tsx
@@ -0,0 +1,20 @@
+import style from "./partners.module.scss";
+
+export default function Partners() {
+  const partners = [
+    "/icons/logo/kta.svg",
+    "/icons/logo/rm.svg",
+    "/icons/logo/fin-serv.svg",
+    "/icons/logo/exalty.svg",
+  ];
+
+  return (
+    <div className={style.container}>
+      {partners.map((partner) => (
+        <section
+          style={{ background: `url(${partner}) no-repeat center`, height: '3.5rem' }}
+        ></section>
+      ))}
+    </div>
+  );
+}
