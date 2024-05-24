c8f526c71f8756946f2eac2526943613bc885442
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-24 22:19:03 +0300

    fising

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,14 +2,14 @@ import Bloks from "@/components/bloks/Bloks";
 import DetailsPart from "@/components/forHomePage/DetailsPart";
 import MainScreen from "@/components/forHomePage/MainScreen";
 import Partners from "@/components/partners/Partners";
-import RunningText from "@/components/runningText/RunningText";
+// import RunningText from "@/components/runningText/RunningText";
 import PartServicesForHome from "@/components/services/PartServicesForHome";
 
 export default function HomePage() {
   return (
     <>
       <MainScreen />
-      <RunningText />
+      {/* <RunningText /> */}
       <DetailsPart />
       <Bloks />
       <PartServicesForHome />
