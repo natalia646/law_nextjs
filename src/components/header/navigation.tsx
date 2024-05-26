f2c806d1c5e243243eb90f252ac8ee08f8ec3c39
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-26 18:55:40 +0300

    create mobile style

--- src/components/header/navigation.tsx
+++ src/components/header/navigation.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { Link } from "@/navigation";
-import styled from "./allHeader.module.css";
+import styled from "./navigation.module.css";
 import { usePathname } from "next/navigation";
 import { NavigationType } from "@/global";
+import { useState } from "react";
+import BurgerMenu from "./burgerMenu";
 
 export default function Navigation({
   navigation,
@@ -11,20 +13,30 @@ export default function Navigation({
 }) {
   const path = usePathname();
   const acticvePath = path.slice(4);
+  const [openMobileMenu, setOpenMobile] = useState(false);
 
   return (
-    <nav className={styled.navigation}>
-      {navigation.map((item, i) => (
-        <Link
-          href={`/${item.path}`}
-          key={i}
-          className={`${styled.link} ${
-            acticvePath === item.path ? styled.active : ""
-          }`}
-        >
-          {item.link}
-        </Link>
-      ))}
-    </nav>
+    <>
+      <nav
+        className={`${openMobileMenu ? styled.mobile_menu : styled.navigation}`}
+      >
+        {navigation.map((item, i) => (
+          <Link
+            href={`/${item.path}`}
+            key={i}
+            onClick={() => setOpenMobile(false)}
+            className={`${styled.link} ${
+              acticvePath === item.path ? styled.active : ""
+            }`}
+          >
+            {item.link}
+          </Link>
+        ))}
+      </nav>
+      <BurgerMenu
+        openMobileMenu={openMobileMenu}
+        setOpenMobile={setOpenMobile}
+      />
+    </>
   );
 }
