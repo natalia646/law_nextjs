2a6a9301b70e22cd86b79f875cc233778fed8d6b
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-27 18:08:20 +0300

    running text

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,18 +2,18 @@ import Bloks from "@/components/bloks/Bloks";
 import DetailsPart from "@/components/forHomePage/DetailsPart";
 import MainScreen from "@/components/forHomePage/MainScreen";
 import Partners from "@/components/partners/Partners";
-// import RunningText from "@/components/runningText/RunningText";
+import RunningText from "@/components/runningText/RunningText";
 import PartServicesForHome from "@/components/services/PartServicesForHome";
 
 export default function HomePage() {
   return (
     <>
       <MainScreen />
-      {/* <RunningText /> */}
+      <RunningText />
       <DetailsPart />
       <Bloks />
       <PartServicesForHome />
-      <Partners/>
+      <Partners />
     </>
   );
 }
