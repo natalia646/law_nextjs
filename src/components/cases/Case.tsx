14e306eeb31005118c9ea6493fd4f80ea1c002ce
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-27 19:58:17 +0300

    styled cases

--- src/components/cases/Case.tsx
+++ src/components/cases/Case.tsx
@@ -1,12 +1,20 @@
 import { Link } from "@/navigation";
 import React from "react";
 import { CaseType } from "@/global";
+import Image from "next/image";
+import style from './case.module.scss'
 
-export default function Case({ id, cases }: { id: number, cases: CaseType[] }) {
-    const {title} = cases[id]
-    
+export default function Case({ id, cases }: { id: number; cases: CaseType[] }) {
+  const { title } = cases[id];
+
   return (
-    <Link href={`/cases/${id}`}>
+    <Link href={`/cases/${id}`} className={style.block}>
+      <Image
+        src={`/cases/case${id}.webp`}
+        alt="photo"
+        width={212}
+        height={278}
+      ></Image>
       <p>{title}</p>
     </Link>
   );
