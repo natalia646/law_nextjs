1a292c5bca89254a93424516e0467c7d54c23b71
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-05-29 17:17:19 +0300

    create layout for cases

---
+++ src/app/[locale]/cases/[id]/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { getMessages } from "next-intl/server";
+import { CaseType } from "@/global";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: number };
+}): Promise<Metadata> {
+  const messages = await getMessages();
+  const { id } = params;
+  if (!Array.isArray(messages.Cases)) {
+    return {};
+  }
+  const casesList: CaseType[] = messages.Cases;
+
+  return {
+    title: "Tedalini - " + casesList[id].title,
+    description: casesList[id].title
+  };
+}
+
+export default function ServiceLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>
+    {children}
+    </div>;
+}
