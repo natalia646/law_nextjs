5577cd8ff555342227b5bb3f47ca032dc058624e
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-01 11:00:56 +0300

    added style to certif

--- src/app/[locale]/certificates/page.tsx
+++ src/app/[locale]/certificates/page.tsx
@@ -1,11 +1,12 @@
 import Certificates from "@/components/certificates/Certificates";
 import getMembersList from "@/functions/getMembersList";
+import style from "./certificates.module.css";
 
 export default function CertificatesPage() {
   const team = getMembersList();
 
   return (
-    <section>
+    <section className={style.container}>
       {team.map((member) => (
         <Certificates certificates={member.certificates} key={member.id} />
       ))}
