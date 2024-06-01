5699ad45e25a66840d0371d9364a25c7a1f86f76
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-01 11:24:19 +0300

    added title

--- src/app/[locale]/certificates/page.tsx
+++ src/app/[locale]/certificates/page.tsx
@@ -1,15 +1,19 @@
 import Certificates from "@/components/certificates/Certificates";
 import getMembersList from "@/functions/getMembersList";
 import style from "./certificates.module.css";
+import { useTranslations } from "next-intl";
 
 export default function CertificatesPage() {
   const team = getMembersList();
-
+  const t = useTranslations();
   return (
     <section className={style.container}>
-      {team.map((member) => (
-        <Certificates certificates={member.certificates} key={member.id} />
-      ))}
+      <h2>{t("CertificatesPage")}</h2>
+      <div className={style.certificates}>
+        {team.map((member) => (
+          <Certificates certificates={member.certificates} key={member.id} />
+        ))}
+      </div>
     </section>
   );
 }
