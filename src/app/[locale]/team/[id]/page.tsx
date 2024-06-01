aef94180ecdafd777bac9b395e95b14412033fbf
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-01 10:42:36 +0300

    create page and components certificates

--- src/app/[locale]/team/[id]/page.tsx
+++ src/app/[locale]/team/[id]/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import style from "./member.module.scss";
 import { useTranslations } from "next-intl";
 import NetworksOfMember from "@/components/team/NetworksOfMember";
+import Certificates from "@/components/certificates/Certificates";
 
 export default function MemberPage({ params }: { params: { id: number } }) {
   const team = getMembersList();
@@ -11,27 +12,30 @@ export default function MemberPage({ params }: { params: { id: number } }) {
   const { name, position, description, image, certificates } = team[id];
 
   return (
-    <article className={style.container}>
-      <section>
-        <p className={style.h5}>{t("name")}</p>
-        <p className={style.h6}>{name}</p>
+    <>
+      <article className={style.container}>
+        <section>
+          <p className={style.h5}>{t("name")}</p>
+          <p className={style.h6}>{name}</p>
 
-        <p className={style.h5}>{t("position")}</p>
-        <p className={style.h6}>{position}</p>
+          <p className={style.h5}>{t("position")}</p>
+          <p className={style.h6}>{position}</p>
 
-        <p className={style.h5}>{t("description")}</p>
-        <p className={style.h6}>{description}</p>
+          <p className={style.h5}>{t("description")}</p>
+          <p className={style.h6}>{description}</p>
 
-        <p className={style.h5}>{t("networks")}</p>
-      <NetworksOfMember item={team[id]}/>
-      </section>
-      <Image
-        src={`/team/${image}`}
-        alt={name}
-        className={style.image}
-        width="700"
-        height="700"
-      />
-    </article>
+          <p className={style.h5}>{t("networks")}</p>
+          <NetworksOfMember item={team[id]} />
+        </section>
+        <Image
+          src={`/team/${image}`}
+          alt={name}
+          className={style.image}
+          width="700"
+          height="700"
+        />
+      </article>
+      <Certificates certificates = {certificates}/>
+    </>
   );
 }
---
+++ src/components/certificates/Certificates.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import style from "./certificates.module.css";
+
+export default function Certificates({
+  certificates,
+}: {
+  certificates: string[];
+}) {
+  return (
+    <>
+      {certificates.map((item, i) => (
+        <Image
+          src={`/certificates/${item}`}
+          alt="certificates"
+          width={200}
+          height={100}
+          key={i}
+          className={style.image}
+        ></Image>
+      ))}
+    </>
+  );
+}
