609ffed1b3f981ea51d7f9468f68df67c4ea7f6c
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-04 17:23:32 +0300

    fix

--- src/components/team/MemberOfTeam.tsx
+++ src/components/team/MemberOfTeam.tsx
@@ -15,14 +15,14 @@ export default function MemberOfTeam({ item }: Props) {
       <div className={style.image_block}>
         <Link href={`/team/${id}`} aria-label="Read more about member of team">
           <section
-            className={style.section}
+            className={style.photo}
             style={{
               backgroundImage: `url(/team/${image})`,
-              backgroundSize: "cover",
-              width: "100%",
-              maxWidth: "305px",
-              minHeight: "297px",
-              height: "100%",
+              backgroundSize: "cover"
+              // width: "100%",
+              // maxWidth: "305px",
+              // minHeight: "297px",
+              // height: "100%",
             }}
           ></section>
         </Link>
