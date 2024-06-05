c96f31afaafd2482189aec673a0f96cf508b5371
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-05 13:30:31 +0300

    chenge member page

--- src/app/[locale]/team/[id]/page.tsx
+++ src/app/[locale]/team/[id]/page.tsx
@@ -1,52 +1,30 @@
 import getMembersList from "@/functions/getMembersList";
-import Image from "next/image";
 import style from "./member.module.scss";
 import { useTranslations } from "next-intl";
-import NetworksOfMember from "@/components/team/NetworksOfMember";
 import Certificates from "@/components/certificates/Certificates";
+import MemberDescription from "@/components/team/MemberDescription";
 
 export default function MemberPage({ params }: { params: { id: number } }) {
   const team = getMembersList();
-  const { id } = params;
   const t = useTranslations("MemberPage");
-  const tr = useTranslations("TeamPage");
-  const { name, position, description, image, certificates } = team[id];
+  const { id } = params;
+  const memmer = team[id];
+  const { image, certificates } = memmer;
 
   return (
     <section className={style.container}>
-      <article className={style.wrapper}>
-        <section>
-          <h2>{tr("our-team")}</h2>
-          <p className={style.h5}>{t("name")}</p>
-          <p className={style.h6}>{name}</p>
-
-          <p className={style.h5}>{t("position")}</p>
-          <p className={style.h6}>{position}</p>
-
-          <p className={style.h5}>{t("description")}</p>
-          <p className={style.h6}>{description}</p>
-
-          <p className={style.h5}>{t("networks")}</p>
-          <NetworksOfMember item={team[id]} />
-        </section>
-        <div className={style.images_wrapper}>
-          <Image
-            src={"/icons/gray-backgr.svg"}
-            alt="fon"
-            width={219}
-            height={80}
-          />
-          <Image
-            src={`/team/${image}`}
-            alt={name}
-            className={style.image}
-            width="700"
-            height="700"
-          />
-        </div>
-
+      <div className={style.wrapper}>
+        <h2 className={style.title}>{t("our-team")}</h2>
+        <span className={style.color}></span>
+        <MemberDescription member={memmer} />
+        <section
+          style={{
+            backgroundImage: `url(/team/${image})`,
+          }}
+          className={style.photo}
+        ></section>
         <h2>{t("certificates")}</h2>
-      </article>
+      </div >
 
       <div className={style.certificates}>
         <Certificates certificates={certificates} />
