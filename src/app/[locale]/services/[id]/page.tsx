01b60f943a6a4ad98707d3db7c0497364868c872
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-06 19:52:58 +0300

    added style to service page

--- src/app/[locale]/services/[id]/page.tsx
+++ src/app/[locale]/services/[id]/page.tsx
@@ -1,8 +1,20 @@
 import getServerList from "@/functions/getServicesList";
+import style from './servicePage.module.scss'
+import getCorrectNumber from "@/functions/getCorrectNumberOfServices";
 
 export default function ServicePage({ params }: { params: { id: number } }) {
   const services = getServerList();
   const { id } = params;
+  const { title } = services[id];
 
-  return <h2>{services[id].title}</h2>;
+  return (
+    <section className={style.container}>
+      <div className={style.title_wrapper}>
+        <h2>{title}</h2>
+        <div className={style.color}>
+          <p>{getCorrectNumber({ id })}</p>
+        </div>
+      </div>
+    </section>
+  );
 }
