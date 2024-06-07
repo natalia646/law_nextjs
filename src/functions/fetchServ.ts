39beb137900740cc257a5552fc723122b97cd29b
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-07 11:58:53 +0300

    api services

---
+++ src/functions/fetchServ.ts
@@ -0,0 +1,17 @@
+async function getData() {
+  const res = await fetch(
+    "https://64ca66e8700d50e3c704da5c.mockapi.io/api/va/services"
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function fetchServ() {
+  const data = await getData();
+
+  return data;
+}
