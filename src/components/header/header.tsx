fcad001e6d69ce2e67466f45bf4d276f29f61857
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-07 19:40:19 +0300

    footer response

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,10 @@
 import { useTranslations } from "next-intl";
 import LangSwitcher from "./local";
 import styled from "./header.module.css";
-import TedaliniLogo from "../tedaliniLogo/TedaliniLogo";
 import Navigation from "./navigation";
 import { Link } from "@/navigation";
 import getNavigation from "@/functions/getNavigation";
+import Image from "next/image";
 
 export default function Header() {
   const t = useTranslations();
@@ -12,8 +12,15 @@ export default function Header() {
 
   return (
     <header className={styled.header}>
-      <TedaliniLogo />
-      <Navigation navigation = {navigation}/>
+      <Link href={`/`}>
+        <Image
+          src={"/icons/logo/tedalini.svg"}
+          alt="tedalini_logo"
+          width={145}
+          height={36}
+        ></Image>
+      </Link>
+      <Navigation navigation={navigation} />
       <Link href={`/contacts`} className={styled.button}>
         {t("ContactPage")}
       </Link>
