6ecd4354b07e72698993274303ef779ef69aec87
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-10 01:56:16 +0300

    perfect

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -7,74 +7,76 @@ export default function Form({ locale }: { locale: string }) {
   const services = getServicesList();
 
   return (
-    <form
-      action="https://formsubmit.co/tedalini.cy@gmail.com"
-      method="POST"
-      className={style.form}
-    >
-      <label htmlFor="name">{t("form.name")}:</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        required
-        placeholder="John Carter"
-      
-      ></input>
-
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        required
-        placeholder="example@email.com"
-      ></input>
-
-      <label htmlFor="phone">{t("form.phone")}:</label>
-      <input
-        type="tel"
-        name="phone"
-        id="phone"
-        required
-        placeholder="(123) 456 - 789"
-      ></input>
-
-      <label htmlFor="action">{t("form.service")}:</label>
-      <select id="action" name="action" required>
-        {services.map((item) => (
-          <option value={item.title} key={item.id}>
-            {item.title}
+    <section className={style.container}>
+      <form
+        action="https://formsubmit.co/tedalini.cy@gmail.com"
+        method="POST"
+        className={style.form}
+      >
+        <label htmlFor="name">{t("form.name")}:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          placeholder="John Carter"
+        
+        ></input>
+  
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          placeholder="example@email.com"
+        ></input>
+  
+        <label htmlFor="phone">{t("form.phone")}:</label>
+        <input
+          type="tel"
+          name="phone"
+          id="phone"
+          required
+          placeholder="(123) 456 - 789"
+        ></input>
+  
+        <label htmlFor="action">{t("form.service")}:</label>
+        <select id="action" name="action" required>
+          {services.map((item) => (
+            <option value={item.title} key={item.id}>
+              {item.title}
+            </option>
+          ))}
+          <option key={services.length}  value="other">
+            {t("form.other")}
           </option>
-        ))}
-        <option key={services.length}  value="other">
-          {t("form.other")}
-        </option>
-      </select>
-
-      <label htmlFor="message">{t("form.message")}:</label>
-      <textarea
-        id="message"
-        name="message"
-        placeholder={t("form.message-placeholder")}
-      ></textarea>
-
-      <label htmlFor="submit"></label>
-      <input
-        type="submit"
-        value={t("connect")}
-        className={style.button}
-        id="submit"
-      ></input>
-
-      <input
-        type="hidden"
-        name="_next"
-        // http://localhost:3000/uk/thanks
-        value={`http://tedalini.com/${locale}/thanks`}
-      ></input>
-      <input type="hidden" name="_captcha" value="false"></input>
-      <input type="hidden" name="_template" value="table"></input>
-    </form>
+        </select>
+  
+        <label htmlFor="message">{t("form.message")}:</label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder={t("form.message-placeholder")}
+        ></textarea>
+  
+        <label htmlFor="submit"></label>
+        <input
+          type="submit"
+          value={t("connect")}
+          className={style.button}
+          id="submit"
+        ></input>
+  
+        <input
+          type="hidden"
+          name="_next"
+          // http://localhost:3000/uk/thanks
+          value={`http://tedalini.com/${locale}/thanks`}
+        ></input>
+        <input type="hidden" name="_captcha" value="false"></input>
+        <input type="hidden" name="_template" value="table"></input>
+      </form>
+    </section>
   );
 }
--- src/components/sceleton/CaseSceleton.tsx
+++ src/components/sceleton/CaseSceleton.tsx
@@ -5,7 +5,7 @@ export default function CaseSceleton() {
         width: "100%",
         maxWidth: "800px",
         minHeight: "150px",
-        backgroundColor: "rgba(158, 158, 158, 0.149)",
+        backgroundColor: "var(--sceleton-color)",
       }}
     ></div>
   );
