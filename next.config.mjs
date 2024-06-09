dc1079b3289544d27fb0e484e5d13cb61d1c0bc7
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-09 19:38:11 +0300

    config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,13 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    domains: ["i.ibb.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+      },
+    ],
   },
 };
 
