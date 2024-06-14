cf472f53db968d733061bdbf8db72010214d93b0
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-14 14:57:40 +0300

    create cont page

--- src/components/form/ContactsPage.tsx
+++ src/components/form/ContactsPage.tsx
@@ -1,8 +1,13 @@
+
 import Contacts from "./Contacts";
 import Form from "./Form";
 import style from "./contacts.module.scss";
 
+
 export default function ContactsPage() {
+
+
+
   return (
     <div className={style.container_page}>
       <Contacts />
