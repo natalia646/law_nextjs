2505dbceeaf9f25caa5f19daba9be910de01a7dc
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-14 15:22:44 +0300

    Revert "create cont page"
    This reverts commit cf472f53db968d733061bdbf8db72010214d93b0.

--- src/components/form/ContactsPage.tsx
+++ src/components/form/ContactsPage.tsx
@@ -1,13 +1,8 @@
-
 import Contacts from "./Contacts";
 import Form from "./Form";
 import style from "./contacts.module.scss";
 
-
 export default function ContactsPage() {
-
-
-
   return (
     <div className={style.container_page}>
       <Contacts />
