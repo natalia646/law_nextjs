fc6fa069d846845ba60751f1c049e18836a770bd
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-06-15 13:29:11 +0300

    resend

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+import EmailTemplate from "../../../components/form/EmailTemplate";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const { name, email, phone, action, message } = body;
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: `Tedalini <info@tedalini.com>`, 
+      to: email,
+      subject: "Message from Portfolio",
+      react: EmailTemplate({ name, email, phone, action, message })
+    });
+
+    if (error) {
+      return NextResponse.json(
+        { message: "Email sending failed", error },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Email sent successfully", data },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return NextResponse.json(
+      { message: "Failed to send email", error },
+      { status: 500 }
+    );
+  }
+}
