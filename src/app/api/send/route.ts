a3d452b1ed7d3f44d50adcc0b10c579f3c3c7182
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-07-13 14:33:57 +0300

    try set up telegram

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,38 +1,49 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Resend } from "resend";
-import EmailTemplate from "../../../components/form/EmailTemplate";
+// import { NextRequest, NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import EmailTemplate from "../../../components/form/EmailTemplate";
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
+// export async function POST(req: NextRequest) {
+//   const body = await req.json();
 
-  const { name, email, phone, action, message } = body;
+//   const { name, email, phone, action, message } = body;
 
-  const resend = new Resend(process.env.RESEND_API_KEY);
+//   const resend = new Resend(process.env.RESEND_API_KEY);
 
-  try {
-    const { data, error } = await resend.emails.send({
-      from: `Tedalini <info@tedalini.com>`, 
-      to: email,
-      subject: "Tedalini Consulting LTD",
-      react: EmailTemplate({ name, email, phone, action, message })
-    });
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: `Tedalini <info@tedalini.com>`,
+//       to: email,
+//       subject: "Tedalini Consulting LTD",
+//       react: EmailTemplate({ name, email, phone, action, message })
+//     });
 
-    if (error) {
-      return NextResponse.json(
-        { message: "Email sending failed", error },
-        { status: 400 }
-      );
+//     if (error) {
+//       return NextResponse.json(
+//         { message: "Email sending failed", error },
+//         { status: 400 }
+//       );
+//     }
+
+//     return NextResponse.json(
+//       { message: "Email sent successfully", data },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     return NextResponse.json(
+//       { message: "Failed to send email", error },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+import axios from "axios";
+export default async function sendMessage(telegramMessage: string) {
+  await axios.post(
+    `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
+    {
+      chat_id: process.env.TELEGRAM_ID,
+      text: telegramMessage,
     }
-
-    return NextResponse.json(
-      { message: "Email sent successfully", data },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return NextResponse.json(
-      { message: "Failed to send email", error },
-      { status: 500 }
-    );
-  }
+  );
 }
---
+++ src/components/form/Form.tsx
@@ -0,0 +1,116 @@
+"use client";
+import getServicesList from "@/functions/getServicesList";
+import style from "./form.module.scss";
+import { FormEvent, useState } from "react";
+import { toast } from "react-hot-toast";
+import getLabel from "./getLabel";
+import sendMessage from "@/app/api/send/route";
+
+export default function Form() {
+  const services = getServicesList();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [action, setAction] = useState(services[0]?.title);
+  const [message, setMessage] = useState("");
+  const [isSending, setIsSending] = useState<boolean>(false);
+
+  const handleSubmit = async () => {
+    try {
+      const telegramMessage = `
+              Name: ${name}
+              Email: ${email}
+              Phone: ${phone}
+              Service: ${action}
+              Message: ${message}
+              `;
+     await sendMessage(telegramMessage);
+      toast.success("Email Sent Successfully!");
+    } catch (e) {
+      console.log(e);
+      toast.error("There was a problem sending email. Pls try again!");
+    }
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <label htmlFor="name">{getLabel("name")}:</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        required
+        placeholder="John Carter"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      ></input>
+
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        required
+        placeholder="example@email.com"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      ></input>
+
+      <label htmlFor="phone">{getLabel("phone")}:</label>
+      <input
+        type="tel"
+        name="phone"
+        id="phone"
+        required
+        placeholder="(123) 456 - 789"
+        value={phone}
+        onChange={(e) => {
+          setPhone(e.target.value);
+        }}
+      ></input>
+
+      <label htmlFor="action">{getLabel("service")}:</label>
+      <select
+        id="action"
+        name="action"
+        required
+        value={action}
+        onChange={(e) => {
+          setAction(e.target.value);
+        }}
+      >
+        {services.map((item) => (
+          <option value={item.title} key={item.id}>
+            {item.title}
+          </option>
+        ))}
+        <option key={services.length} value="other">
+          {getLabel("other")}
+        </option>
+      </select>
+
+      <label htmlFor="message">{getLabel("message")}:</label>
+      <textarea
+        id="message"
+        name="message"
+        placeholder={getLabel("message-placeholder")}
+        value={message}
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      ></textarea>
+
+      <label htmlFor="submit"></label>
+      <input
+        type="submit"
+        value={getLabel("connect")}
+        className={style.button}
+        id="submit"
+      ></input>
+    </form>
+  );
+}
